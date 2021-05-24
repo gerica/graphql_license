f1502715d6ceb4bc9ab6b18374920a0849b32e71
gerica <rogeriogerica@gmail.com>
2021-05-24 11:10:28 -0300

    new version

--- src/graphql/resolvers/randomResolver.js
+++ src/graphql/resolvers/randomResolver.js
@@ -1,7 +1,7 @@
 import { RandomDie } from '../../api/services/index.js';
 
 const query = {
-  versionLicense: () => 'Radiolife version license: 1.0.0-rc-03',
+  versionLicense: () => 'Radiolife version license: 1.0.0-rc-01',
   getDie: (_, { numSides }) => new RandomDie(numSides || 6),
 };
 const mutation = {};
